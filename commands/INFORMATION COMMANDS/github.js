27d38e0e06a21743883e289ebbc30f98e284122b
dev-REDZONE <78139545+dev-REDZONE@users.noreply.github.com>
2021-08-25 18:53:25 +0300

    Update github.js

--- commands/INFORMATION COMMANDS/github.js
+++ commands/INFORMATION COMMANDS/github.js
@@ -12,16 +12,16 @@ module.exports = {
 	run: async (client, message, args, user, text, prefix) => {
 			message.channel.send(new MessageEmbed()
 				.setTitle(`This Bot is made by \`JANO#4534\` and **this** is the Source Code link to this Bot`)
-				.setURL("https://github.com/Devjano")
+				.setURL("https://github.com/dev-REDZONE")
 				.addField("Discord.js: ", "[\`v12.5.1\`](https://discord.js.org)", true)
 				.addField("FFMPEG: ", "\`v4.1.6-1\`", true)
 				.addField("Node.js: ", "[\`v15.3.4\`](https://nodejs.org/en/)", true)
 				.addField("Distube: ", "[\`v2.8.7\`](https://distube.js.org)")
-				.addField("Source Code. ", "Don't just use the source for yourself,... please [ invite ](https://discord.com/api/oauth2/authorize?client_id=828921463348264982&permissions=0&scope=bot) me too![\`Click here\`](https://discord.com/api/oauth2/authorize?client_id=828921463348264982&permissions=0&scope=bot)")
-			        .addField("Source Code. ", "[ My git hub ](https://github.com/Devjano) link git hub ![\`Click here\`](https://github.com/Devjano)")
+				.addField("Source Code. ", "Don't just use the source for yourself,... please [ invite ](https://discord.com/api/oauth2/authorize?client_id=843107387326201887&permissions=8&scope=bot) me too![\`Click here\`](https://discord.com/api/oauth2/authorize?client_id=843107387326201887&permissions=8&scope=bot)")
+			        .addField("Source Code. ", "[ My git hub ](https://github.com/dev-REDZONE) link git hub ![\`Click here\`](https://github.com/dev-REDZONE)")
 				
 				.setColor(config.colors.yes)
-				.setFooter("Calms| by: jano", client.user.displayAvatarURL())
+				.setFooter("JOKER STAFF⎮ By: RedZone", client.user.displayAvatarURL())
 			);
 	}
 }
